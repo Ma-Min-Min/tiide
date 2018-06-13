f7495e30e6edd236363a786b3d4cdbba95e4f04c
Ma-Min-Min <minmin12697@gmail.com>
2018-06-13 22:52:59 +0630

    Changed object name from app to myapp

---
+++ start.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+myapp = Flask(__name__)
+
+@myapp.route("/")
+def hello():
+        return "Hello World"
+@myapp.route("/tiide")
+def tiide():
+            return "Welcome to TIIDE World"
+if __name__=="__main__":    
+    app.run()
