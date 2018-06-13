74fb5db7d437c527d956e2b9692d66997e800274
Ma-Min-Min <39396690+Ma-Min-Min@users.noreply.github.com>
2018-06-13 23:01:10 +0630

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,12 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World"
-
-@app.route("/min")
-def min():
-    return "Min Min"
-
-if __name__=="__main__":
-    app.run()
-
+    return "Hello"
